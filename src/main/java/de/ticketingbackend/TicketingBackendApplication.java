2a51742235ef6cf0bae8480bb0cb99f35c5bdedc
Simon Leitl <simon.leitl@esentri.com>
2021-07-27 23:04:52 +0200

    Initial commit

---
+++ src/main/java/de/ticketingbackend/TicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package de.ticketingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingBackendApplication.class, args);
+	}
+
+}
